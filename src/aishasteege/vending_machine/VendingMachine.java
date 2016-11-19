a2c5f8f6ccdee14bc609b48f0d3beec635df49cd
aishasteege <aisha.steege@gmail.com>
2016-11-19 09:46:52 -0600

    Add first test for display output

---
+++ src/aishasteege/vending_machine/VendingMachine.java
@@ -0,0 +1,14 @@
+package aishasteege.vending_machine;
+
+public class VendingMachine {
+
+	public static void main(String[] argvs) {
+        System.out.println("Hello World !");
+    }
+	
+	String PrintDisplay()
+	{
+		return "INSERT COIN";
+	}
+	
+}
