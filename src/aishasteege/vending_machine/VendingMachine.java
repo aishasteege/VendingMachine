26272050fc692d7db886583ad6fdfa255e4c2b2f
aishasteege <aisha.steege@gmail.com>
2016-11-19 11:29:29 -0600

    format code a little bit ... messing with eclipse settings

--- src/aishasteege/vending_machine/VendingMachine.java
+++ src/aishasteege/vending_machine/VendingMachine.java
@@ -4,28 +4,23 @@ import java.text.DecimalFormat;
 
 public class VendingMachine {
 	float current_transaction;
-	
-	VendingMachine(){
+
+	VendingMachine() {
 		current_transaction = 0;
 	}
 
-	String getDisplayString()
-	{
-		DecimalFormat moneyFormat = new DecimalFormat( "0.00" );
-		
-		if ( current_transaction == 0 )
-		{
+	String getDisplayString() {
+		DecimalFormat moneyFormat = new DecimalFormat("0.00");
+
+		if (current_transaction == 0) {
 			return "INSERT COIN";
-		}
-		else 
-		{
+		} else {
 			return "$" + moneyFormat.format(current_transaction);
 		}
 	}
 
-	public void addCoin( Coin coin ) {
-		switch ( coin )
-		{
+	public void addCoin(Coin coin) {
+		switch (coin) {
 		case NICKEL:
 			current_transaction += 0.05f;
 			break;
@@ -33,7 +28,7 @@ public class VendingMachine {
 			current_transaction += 0.10f;
 			break;
 		case QUARTER:
-			current_transaction += 0.25f;	
+			current_transaction += 0.25f;
 			break;
 		default:
 			break;
--- test/aishasteege/vending_machine/VendingMachineTest.java
+++ test/aishasteege/vending_machine/VendingMachineTest.java
@@ -10,47 +10,45 @@ import org.junit.Test;
 
 public class VendingMachineTest {
 
-//	@BeforeClass
-//	public static void setUpBeforeClass() throws Exception {
-//	}
-//
-//	@AfterClass
-//	public static void tearDownAfterClass() throws Exception {
-//	}
-//
-//
-//	@After
-//	public void tearDown() throws Exception {
-//	}
+	// @BeforeClass
+	// public static void setUpBeforeClass() throws Exception {
+	// }
+	//
+	// @AfterClass
+	// public static void tearDownAfterClass() throws Exception {
+	// }
+	//
+	//
+	// @After
+	// public void tearDown() throws Exception {
+	// }
 
 	VendingMachine VendingMachine;
-	
+
 	@Before
-	public void setUp(){
-		VendingMachine = new VendingMachine() ;
-	}
-	
-	@Test
-	public void WhenVendingMachineDisplayStartsWithInsertCoin() {
-		assertEquals( VendingMachine.getDisplayString(), "INSERT COIN");
-	}
-	
-	@Test
-	public void WhenQuarterIsAddedShowTwentyFive(){
-		VendingMachine.addCoin( Coin.QUARTER );		
-		assertEquals( VendingMachine.getDisplayString(), "$0.25");
-	}
-	
-	@Test
-	public void WhenValidCoinsAreAddedShowCompinedValue(){
-		VendingMachine.addCoin( Coin.NICKEL );	
-		assertEquals( VendingMachine.getDisplayString(), "$0.05");
-		VendingMachine.addCoin( Coin.DIME );	
-		assertEquals( VendingMachine.getDisplayString(), "$0.15");
-		VendingMachine.addCoin( Coin.QUARTER );		
-		assertEquals( VendingMachine.getDisplayString(), "$0.40");
+	public void setUp() {
+		VendingMachine = new VendingMachine();
 	}
 
-	
-	
+	@Test
+	public void WhenVendingMachineDisplayStartsWithInsertCoin() {
+		assertEquals(VendingMachine.getDisplayString(), "INSERT COIN");
+	}
+
+	@Test
+	public void WhenQuarterIsAddedShowTwentyFive() {
+		VendingMachine.addCoin(Coin.QUARTER);
+		assertEquals(VendingMachine.getDisplayString(), "$0.25");
+	}
+
+	@Test
+	public void WhenValidCoinsAreAddedShowCompinedValue() {
+		VendingMachine.addCoin(Coin.NICKEL);
+		assertEquals(VendingMachine.getDisplayString(), "$0.05");
+		VendingMachine.addCoin(Coin.DIME);
+		assertEquals(VendingMachine.getDisplayString(), "$0.15");
+		VendingMachine.addCoin(Coin.QUARTER);
+		assertEquals(VendingMachine.getDisplayString(), "$0.40");
+	}
+
 }
