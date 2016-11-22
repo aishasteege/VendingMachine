a4a4158177b9920e4022c0c1a3ec70c795243321
aishasteege <aisha.steege@gmail.com>
2016-11-22 12:05:46 -0600

    refactor coin return

--- src/aishasteege/vending_machine/Coin.java
+++ src/aishasteege/vending_machine/Coin.java
@@ -2,20 +2,27 @@ package aishasteege.vending_machine;
 
 public enum Coin
 {
-	PENNY(0), NICKEL(1), DIME(2), QUARTER(3);
+	PENNY(0, "(1)"), NICKEL(1, "(5)"), DIME(2, "(10)"), QUARTER(3, "(25)");
 
 	public static int NUM_COINS = 4;
 	public static Coin[] COINS_INDEXED = new Coin[] { PENNY, NICKEL, DIME, QUARTER };
 
 	private int m_idx;
+	private String m_icon;
 
-	private Coin(int idx)
+	private Coin(int idx, String string_icon)
 	{
 		m_idx = idx;
+		m_icon = string_icon;
 	}
 
 	public int getIdx()
 	{
 		return m_idx;
+	}
+	
+	public String getIcon()
+	{
+		return m_icon;
 	}
 }
--- src/aishasteege/vending_machine/CoinMechanism.java
+++ src/aishasteege/vending_machine/CoinMechanism.java
@@ -4,20 +4,12 @@ import java.text.DecimalFormat;
 
 public class CoinMechanism
 {
-	int[] current_transaction_coins;
-	float current_transaction;
-	String coinReturn;
+	int[] current_transaction_coins = new int[Coin.NUM_COINS];
+	float current_transaction = 0.0f;
+	String coinReturn = new String();
 
 	public CoinMechanism()
 	{
-		current_transaction = 0.0f;
-		coinReturn = "";
-
-		current_transaction_coins = new int[Coin.NUM_COINS];
-		for (int i = 0; i < Coin.NUM_COINS; i++)
-		{
-			current_transaction_coins[i] = 0;
-		}
 	}
 
 	public boolean isEmpty()
@@ -55,25 +47,17 @@ public class CoinMechanism
 
 	public void pressCoinReturn()
 	{
-		while (current_transaction_coins[Coin.NICKEL.getIdx()] > 0)
-		{
-			coinReturn += "(5)";
-			current_transaction_coins[Coin.NICKEL.getIdx()]--;
-		}
-
-		while (current_transaction_coins[Coin.DIME.getIdx()] > 0)
-		{
-			coinReturn += "(10)";
-			current_transaction_coins[Coin.DIME.getIdx()]--;
-		}
-
-		while (current_transaction_coins[Coin.QUARTER.getIdx()] > 0)
-		{
-			coinReturn += "(25)";
-			current_transaction_coins[Coin.QUARTER.getIdx()]--;
-		}
-
+		returnCoin(Coin.NICKEL);
+		returnCoin(Coin.DIME);
+		returnCoin(Coin.QUARTER);
 		current_transaction = 0.0f;
+	}
+
+	private void returnCoin(Coin coin)
+	{
+		int count = current_transaction_coins[coin.getIdx()];
+		coinReturn += new String(new char[count]).replace("\0", coin.getIcon());
+		current_transaction_coins[coin.getIdx()] = 0;
 	}
 
 	public String getCoinReturnString()
@@ -108,10 +92,8 @@ public class CoinMechanism
 				current_transaction -= 0.05f;
 			}
 
-			for (int i = 0; i < Coin.NUM_COINS; i++)
-			{
-				current_transaction_coins[i] = 0;
-			}
+			java.util.Arrays.fill(current_transaction_coins, 0);
+
 			return true;
 		}
 		return false;
